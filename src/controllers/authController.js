32512eb78c385ea7ba983ad02bd5dd5dc9ae9233
Maroc <marcos.valentee20@gmail.com>
2021-09-04 09:45:34 -0400

    pwd

---
+++ src/controllers/authController.js
@@ -0,0 +1,46 @@
+const express = require('express');
+
+const User = require('../models/user');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
+const router = express.Router()
+
+router.post('/register', async (req, res) => {
+    const {email} = req.body
+
+    try {
+        if (await User.findOne({ email }))
+            return res.status(400).send({ error: "user already exists"})
+
+        const user = await User.create(req.body)
+
+        user.password = undefined
+
+        return res.send({ user })
+
+    } catch (err) {
+        return res.status(400).send({ error: 'Registration failed' })
+    }
+
+})
+
+router.post('/authenticate', async (req, res) => {
+    const {email, password} = req.body
+
+    const user = await User.findOne({ email }).select('+password') 
+
+    if(!user)
+        return res.status(400).send({ error: "User not found" })
+
+    if(!await bcrypt.compare(password, user.password))
+        return res.status(400).send({ error: "Invalid password" })
+
+    user.password = undefined
+
+    const token = jwt.sign({id: user.id}, )
+    
+    res.send({ user })
+})
+
+module.exports = app => app.use('/auth', router)
---
+++ src/index.js
@@ -0,0 +1,19 @@
+const express = require('express')
+
+
+const app = express();
+
+
+// para entender quando eu envia informaçoes para api em json
+app.use(express.json())
+// para entender quando passar parametros via urls e decodificar 
+app.use(express.urlencoded({ extended: false }))
+
+
+// req = dados da requests (param, token)
+// res = object to response for the user when him access this route
+// referenced
+require('./controllers/authController')(app)
+
+
+app.listen(3000)
